81c9612090d175281321850a45d39a3654120ff4
Roger Allen <rallen@gmail.com>
2021-06-26 17:11:17 -0700

    refactoring functions

--- Core/Src/reverb.c
+++ Core/Src/reverb.c
@@ -11,26 +11,21 @@
 #include "reverb.h"
 #include "string.h"
 
-float do_comb0(reverb_state_t *self, float v);
-float do_comb1(reverb_state_t *self, float v);
-float do_comb2(reverb_state_t *self, float v);
-float do_comb3(reverb_state_t *self, float v);
-float do_allp0(reverb_state_t *self, float v);
-float do_allp1(reverb_state_t *self, float v);
-float do_allp2(reverb_state_t *self, float v);
+float comb_filter(float *buf, int *idx, float gain, int lim, float v);
+float allpass_filter(float *buf, int *idx, float gain, int lim, float v);
 
 // ======================================================================
 void reverb_init(reverb_state_t *self, float wet, float delay)
 {
   self->wet = wet;
   self->delay = delay;
-  self->comb0_lim = delay/2.0 * COMB0_LEN;
-  self->comb1_lim = delay/2.0 * COMB1_LEN;
-  self->comb2_lim = delay/2.0 * COMB2_LEN;
-  self->comb3_lim = delay/2.0 * COMB3_LEN;
-  self->allp0_lim = delay/2.0 * ALLP0_LEN;
-  self->allp1_lim = delay/2.0 * ALLP1_LEN;
-  self->allp2_lim = delay/2.0 * ALLP2_LEN;
+  self->comb0_lim = delay/MAX_DELAY * COMB0_LEN;
+  self->comb1_lim = delay/MAX_DELAY * COMB1_LEN;
+  self->comb2_lim = delay/MAX_DELAY * COMB2_LEN;
+  self->comb3_lim = delay/MAX_DELAY * COMB3_LEN;
+  self->allp0_lim = delay/MAX_DELAY * ALLP0_LEN;
+  self->allp1_lim = delay/MAX_DELAY * ALLP1_LEN;
+  self->allp2_lim = delay/MAX_DELAY * ALLP2_LEN;
   memset(&(self->comb0[0]), 0, sizeof(float)*COMB0_LEN);
   memset(&(self->comb1[0]), 0, sizeof(float)*COMB1_LEN);
   memset(&(self->comb2[0]), 0, sizeof(float)*COMB2_LEN);
@@ -59,10 +54,14 @@ void reverb_get_samples(reverb_state_t *self, float *in_samples, float *out_samp
 {
   for(int frame = 0; frame < frame_count; frame++) {
     float sample = in_samples[2*frame]; // just left sample for reverb input
-    float newsample = (do_comb0(self, sample) + do_comb1(self, sample) + do_comb2(self, sample) + do_comb3(self, sample))/4.0f;
-    newsample = do_allp0(self, newsample);
-    newsample = do_allp1(self, newsample);
-    newsample = do_allp2(self, newsample);
+    float newsample = comb_filter(&(self->comb0[0]), &(self->comb0_idx), self->comb0_gain, self->comb0_lim, sample);
+    newsample = comb_filter(&(self->comb1[0]), &(self->comb1_idx), self->comb1_gain, self->comb1_lim, sample);
+    newsample = comb_filter(&(self->comb2[0]), &(self->comb2_idx), self->comb2_gain, self->comb2_lim, sample);
+    newsample = comb_filter(&(self->comb3[0]), &(self->comb3_idx), self->comb3_gain, self->comb3_lim, sample);
+    newsample /= 4;
+    newsample = allpass_filter(&(self->allp0[0]), &(self->allp0_idx), self->allp0_gain, self->allp0_lim, sample);
+    newsample = allpass_filter(&(self->allp1[0]), &(self->allp1_idx), self->allp1_gain, self->allp1_lim, sample);
+    newsample = allpass_filter(&(self->allp2[0]), &(self->allp2_idx), self->allp2_gain, self->allp2_lim, sample);
     newsample = (1.0-self->wet)*sample + self->wet*newsample;
     float newsample1 = (1.0-self->wet)*in_samples[2*frame+1] + self->wet*newsample;
     out_samples[2*frame] = newsample;
@@ -70,76 +69,25 @@ void reverb_get_samples(reverb_state_t *self, float *in_samples, float *out_samp
   }
 }
 
-inline float do_comb0(reverb_state_t *self, float v)
+inline float comb_filter(float *buf, int *idx, float gain, int lim, float v)
 {
-  float new_v = self->comb0[self->comb0_idx]*(self->comb0_gain) + v;
-  self->comb0[self->comb0_idx] = new_v;
-  self->comb0_idx += 1;
-  if (self->comb0_idx == self->comb0_lim) {
-    self->comb0_idx = 0;
+  float new_v = buf[*idx]*gain + v;
+  buf[*idx] = new_v;
+  *idx += 1;
+  if (*idx == lim) {
+    *idx = 0;
   }
   return new_v;
 }
-inline float do_comb1(reverb_state_t *self, float v)
+
+inline float allpass_filter(float *buf, int *idx, float gain, int lim, float v)
 {
-  float new_v = self->comb1[self->comb1_idx]*(self->comb1_gain) + v;
-  self->comb1[self->comb1_idx] = new_v;
-  self->comb1_idx += 1;
-  if (self->comb1_idx == self->comb1_lim) {
-    self->comb1_idx = 0;
-  }
-  return new_v;
-}
-inline float do_comb2(reverb_state_t *self, float v)
-{
-  float new_v = self->comb2[self->comb2_idx]*(self->comb2_gain) + v;
-  self->comb2[self->comb2_idx] = new_v;
-  self->comb2_idx += 1;
-  if (self->comb2_idx == self->comb2_lim) {
-    self->comb2_idx = 0;
-  }
-  return new_v;
-}
-inline float do_comb3(reverb_state_t *self, float v)
-{
-  float new_v = self->comb3[self->comb3_idx]*(self->comb3_gain) + v;
-  self->comb3[self->comb3_idx] = new_v;
-  self->comb3_idx += 1;
-  if (self->comb3_idx == self->comb3_lim) {
-    self->comb3_idx = 0;
-  }
-  return new_v;
-}
-inline float do_allp0(reverb_state_t *self, float v)
-{
-  float feedback = self->allp0[self->allp0_idx] + (-self->allp0_gain) * v;
-  float new_v = feedback*(self->allp0_gain) + v;
-  self->allp0[self->allp0_idx] = new_v;
-  self->allp0_idx += 1;
-  if (self->allp0_idx == self->allp0_lim) {
-    self->allp0_idx = 0;
-  }
-  return new_v;
-}
-inline float do_allp1(reverb_state_t *self, float v)
-{
-  float feedback = self->allp1[self->allp1_idx] + (-self->allp1_gain) * v;
-  float new_v = feedback*(self->allp1_gain) + v;
-  self->allp1[self->allp1_idx] = new_v;
-  self->allp1_idx += 1;
-  if (self->allp1_idx == self->allp1_lim) {
-    self->allp1_idx = 0;
-  }
-  return new_v;
-}
-inline float do_allp2(reverb_state_t *self, float v)
-{
-  float feedback = self->allp2[self->allp2_idx] + (-self->allp2_gain) * v;
-  float new_v = feedback*(self->allp2_gain) + v;
-  self->allp2[self->allp2_idx] = new_v;
-  self->allp2_idx += 1;
-  if (self->allp2_idx == self->allp2_lim) {
-    self->allp2_idx = 0;
+  float feedback = buf[*idx] + (-gain) * v;
+  float new_v = feedback*gain + v;
+  buf[*idx] = new_v;
+  *idx += 1;
+  if (*idx == lim) {
+    *idx = 0;
   }
   return new_v;
 }
